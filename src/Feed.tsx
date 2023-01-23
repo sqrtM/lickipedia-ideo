51a8914612e407df6925ef11e6192402f2342442
Mason Pike <masonapike@gmail.com>
2023-01-23 21:51:30 +0100

    allowed deletions. cleaned. fixed multi-refresh

--- src/Feed.tsx
+++ src/Feed.tsx
@@ -1,39 +1,38 @@
 'use client'
-import React, { useEffect, useState } from 'react';
-import TextareaAutosize from 'react-textarea-autosize';
+import { useEffect, useState } from 'react'
+import TextareaAutosize from 'react-textarea-autosize'
 
-import styles from '../styles/Feed.module.scss';
+import styles from '../styles/Feed.module.scss'
 
-import FeedItem from './FeedItem';
-import RightColumn from './RightColumn';
-import LeftColumn from './LeftColumn';
-import { feedItemType, defaultFeedParams } from './util';
+import FeedItem from './FeedItem'
+import RightColumn from './RightColumn'
+import LeftColumn from './LeftColumn'
+import { feedItemType, defaultFeedParams } from './util'
 
 // why UUID? Because other serialization functions
 // would cause different floating point problems and
 // other weird issues. giving each lick a UUID means
 // it will be easy to tell everything apart. We
 // can also make unique URLs with this later.
-import { v4 as uuidv4 } from 'uuid';
-import { AbcVisualParams, Editor } from 'abcjs';
-import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid'
+import { AbcVisualParams, Editor } from 'abcjs'
+import axios from 'axios'
 
 export interface IFeedState {
-  title: string;
-  key: string;
-  composer: string;
-  Clef: string;
-  music: string;
-  parent: string;
+  title: string
+  key: string
+  composer: string
+  Clef: string
+  music: string
+  parent: string
 
-  history: feedItemType[];
+  history: feedItemType[]
 
-  savedNotation: feedItemType[];
-  savedLicks: string[];
+  savedNotation: feedItemType[]
+  savedLicks: string[]
 
-  editorShown: boolean;
+  editorShown: boolean
 }
-
 
 /*
 TODO : 
@@ -49,7 +48,6 @@ TODO :
 */
 
 export default function Feed(): JSX.Element {
-
   const [feedItemList, setFeedItemList] = useState<IFeedState>({
     title: '',
     key: 'C',
@@ -64,34 +62,20 @@ export default function Feed(): JSX.Element {
   });
 
   useEffect(() => {
-    new Editor(
-      "music",
-      {
-        canvas_id: "paper",
-        warnings_id: "warnings",
-        abcjsParams: defaultFeedParams,
-      });
-  }, [feedItemList.history])
+    new Editor('music', {
+      canvas_id: 'paper',
+      warnings_id: 'warnings',
+      abcjsParams: defaultFeedParams,
+    });
+  }, [feedItemList.editorShown]);
 
   useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/licks")
-      .then((res) => {
-        console.log(res.data)
-        let newhist: feedItemType[] = [];
-        for (let i = 0; i < res.data.length; i++) {
-          newhist.unshift([res.data[i].uuid, res.data[i].music_string, defaultFeedParams, res.data[i].parent, res.data[i].date]);
-        }
-        setFeedItemList({
-          ...feedItemList,
-          history: newhist,
-        })
-        console.log(newhist)
-      })
+    refreshFeed();
   }, [feedItemList.savedNotation]);
 
-  function handleChange(event: { target: { name: string, value: any; } }): void {
-    const target = event.target;
-    const name = target.name;
+  function handleChange(event: { target: { name: string; value: any } }): void {
+    const target = event.target
+    const name = target.name
     setFeedItemList({
       ...feedItemList,
       [name]: event.target.value,
@@ -99,44 +83,54 @@ export default function Feed(): JSX.Element {
   }
 
   // this formats a UUID, a tuneString, the default parameters;
-  // and a locale string which gets sent into the history 
+  // and a locale string which gets sent into the history
   // to be sent into the FeedItems.
-  function handleSubmit(event: { preventDefault: () => void; }) {
-    let newString: string = `T:${feedItemList.title}\nM:4/4\nC:${feedItemList.composer}\nK:${feedItemList.key} clef=${feedItemList.Clef}\n${feedItemList.music}`;
-    let params: AbcVisualParams = defaultFeedParams;
-    let newFeedItem: feedItemType = [uuidv4(), newString, params, feedItemList.parent, new Date().toLocaleString()];
-    axios.post("http://127.0.0.1:8000/api/licks", {
+  function handleSubmit(event: { preventDefault: () => void }) {
+    let newString: string = `T:${feedItemList.title}\nM:4/4\nC:${feedItemList.composer}\nK:${feedItemList.key} clef=${feedItemList.Clef}\n${feedItemList.music}`
+    let params: AbcVisualParams = defaultFeedParams
+    let newFeedItem: feedItemType = [
+      uuidv4(),
+      newString,
+      params,
+      feedItemList.parent,
+      new Date().toLocaleString(),
+    ];
+    axios.post('http://127.0.0.1:8000/api/licks', {
       uuid: newFeedItem[0],
       music_string: newFeedItem[1],
       parent: newFeedItem[3],
       date: newFeedItem[4],
     });
-    alert('The following lick has been submitted: ' + newString);
+    alert('The following lick has been submitted: ' + newString)
     event.preventDefault();
     setFeedItemList({
-      ...feedItemList,    // clear state on submit.
+      ...feedItemList, // clear state on submit.
       title: '',
       key: 'C',
       composer: '',
       Clef: 'treble',
       music: '',
       parent: '',
-      savedNotation: [],
-      savedLicks: [],
+
       editorShown: false,
     });
+    refreshFeed();
   }
 
   // this takes the saved licks from the FeedItem component and sends them to be state,
   // so they can be sent to the RightColumn.
   function retrieveSavedLicks(id: string): void {
-    let newSavedNotation: feedItemType[] = [...feedItemList.savedNotation];
+    let newSavedNotation: feedItemType[] = [...feedItemList.savedNotation]
     if (!feedItemList.savedLicks.includes(id)) {
-      feedItemList.history.forEach(j => { if (j.includes(id)) { newSavedNotation.unshift(j) } })
+      feedItemList.history.forEach((j) => {
+        if (j.includes(id)) {
+          newSavedNotation.unshift(j);
+        }
+      })
       setFeedItemList({
         ...feedItemList,
         savedLicks: [...feedItemList.savedLicks, id],
-        savedNotation: [...newSavedNotation]
+        savedNotation: [...newSavedNotation],
       });
     }
   }
@@ -148,15 +142,34 @@ export default function Feed(): JSX.Element {
   const recieveFork = (fork: feedItemType): void => {
     // lots of string manip to get the 'tune object'
     // to split properly into the different text fields
-    let musicArray = fork[1].split(`\n`, 5);
+    let musicArray = fork[1].split(`\n`, 5)
     setFeedItemList({
       ...feedItemList,
-      title: musicArray[0].split(":")[1],
-      composer: musicArray[2].split(":")[1],
-      key: musicArray[3].split(":")[1].split(" ")[0],
-      Clef: musicArray[3].split("=")[1],
+      title: musicArray[0].split(':')[1],
+      composer: musicArray[2].split(':')[1],
+      key: musicArray[3].split(':')[1].split(' ')[0],
+      Clef: musicArray[3].split('=')[1],
       music: musicArray[4],
       parent: fork[0],
+    });
+  }
+
+  const refreshFeed = (): void => {
+    axios.get('http://127.0.0.1:8000/api/licks').then((res) => {
+      let newhist: feedItemType[] = [];
+      for (let i = 0; i < res.data.length; i++) {
+        newhist.unshift([
+          res.data[i].uuid,
+          res.data[i].music_string,
+          defaultFeedParams,
+          res.data[i].parent,
+          res.data[i].date,
+        ]);
+      }
+      setFeedItemList({
+        ...feedItemList,
+        history: newhist,
+      });
     });
   }
 
@@ -170,40 +183,86 @@ export default function Feed(): JSX.Element {
           <form id={styles.form} onSubmit={handleSubmit}>
             <label>
               <span id={styles.inputTopRow}>
-                <input type='text' name='title' placeholder='title' value={feedItemList.title} onChange={handleChange} />
-                <input type='text' name='key' placeholder='key' value={feedItemList.key} onChange={handleChange} />
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="title"
+                  value={feedItemList.title}
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="key"
+                  placeholder="key"
+                  value={feedItemList.key}
+                  onChange={handleChange}
+                />
               </span>
               <span className={styles.inputRowRow}>
-                <input type='text' name='composer' placeholder='composer' value={feedItemList.composer} onChange={handleChange} />
+                <input
+                  type="text"
+                  name="composer"
+                  placeholder="composer"
+                  value={feedItemList.composer}
+                  onChange={handleChange}
+                />
                 <span id={styles.radio}>
-                  <input type="radio" name='Clef' value='treble' onChange={handleChange} checked={feedItemList.Clef === 'treble'} /> treble
-                  <input type="radio" name='Clef' value='bass' onChange={handleChange} checked={feedItemList.Clef === 'bass'} /> bass
+                  <input
+                    type="radio"
+                    name="Clef"
+                    value="treble"
+                    onChange={handleChange}
+                    checked={feedItemList.Clef === 'treble'}
+                  />{' '}
+                  treble
+                  <input
+                    type="radio"
+                    name="Clef"
+                    value="bass"
+                    onChange={handleChange}
+                    checked={feedItemList.Clef === 'bass'}
+                  />{' '}
+                  bass
                 </span>
               </span>
               <div className={styles.inputRowCol}>
                 <TextareaAutosize
-                  name='music'
+                  name="music"
                   id="music"
-                  placeholder='music'
+                  placeholder="music"
                   value={feedItemList.music}
                   onChange={handleChange}
                   minRows={1}
                 />
                 <span className={styles.parent}>
-                  {feedItemList.parent &&
-                    <input type='textarea' value={feedItemList.parent} readOnly />
-                  }
+                  {feedItemList.parent && (
+                    <input
+                      type="textarea"
+                      value={feedItemList.parent}
+                      readOnly
+                    />
+                  )}
                 </span>
               </div>
             </label>
-            <input type='button' name='editor' value='editor' onClick={() => setFeedItemList({ ...feedItemList, editorShown: !feedItemList.editorShown })} />
-            {feedItemList.editorShown &&
+            <input
+              type="button"
+              name="editor"
+              value="editor"
+              onClick={() =>
+                setFeedItemList({
+                  ...feedItemList,
+                  editorShown: !feedItemList.editorShown,
+                })
+              }
+            />
+            {feedItemList.editorShown && (
               <div>
                 <div id="paper"></div>
-                <div id="warnings" style={{ color: "#BB68FC" }}></div>
+                <div id="warnings" style={{ color: '#BB68FC' }}></div>
               </div>
-            }
-            <input type="submit" value='submit' />
+            )}
+            <input type="submit" value="submit" />
           </form>
         </div>
         <div>
@@ -212,6 +271,7 @@ export default function Feed(): JSX.Element {
             parserParams={defaultFeedParams}
             retrieveSavedLicks={retrieveSavedLicks}
             recieveFork={recieveFork}
+            refresh={refreshFeed}
           />
         </div>
       </div>
@@ -223,5 +283,5 @@ export default function Feed(): JSX.Element {
         />
       </div>
     </div>
-  );
+  )
 }
--- src/FeedItem.tsx
+++ src/FeedItem.tsx
@@ -2,21 +2,24 @@ import styles from '../styles/FeedItem.module.scss'
 import AbcVisualParams from 'abcjs'
 import { feedItemType, renderAbcNotation } from "./util"
 import { useEffect } from 'react';
+import axios from 'axios';
 
 interface IFeedItemProps {
   parserParams: AbcVisualParams.AbcVisualParams,
   retrieveSavedLicks: (i: string) => void;
   recieveFork: (i: feedItemType) => void;
   historyFeed: feedItemType[];
+  refresh: () => void;
 }
 
 export default function FeedItem(FeedItemProps: IFeedItemProps): JSX.Element {
 
-  useEffect(() => { FeedItemProps.historyFeed?.forEach(i => renderAbcNotation(i[0], i[1], i[2])) })
+  useEffect(() => { FeedItemProps.historyFeed?.forEach(i => renderAbcNotation(i[0], i[1], i[2])) });
 
-  const saveLick = (s: string): void => { FeedItemProps.retrieveSavedLicks(s) }
+  const saveLick = (s: string): void => { FeedItemProps.retrieveSavedLicks(s); }
   const handleTranspose = (e: any, i: feedItemType): void => renderAbcNotation(i[0], i[1], { ...i[2], visualTranspose: +e.target.value });
-  const handleFork = (i: feedItemType): void => { FeedItemProps.recieveFork(i) }
+  const handleFork = (i: feedItemType): void => { FeedItemProps.recieveFork(i); }                                     // v temp fix v
+  const handleDelete = (i: string): void => { axios.delete("http://127.0.0.1:8000/api/licks", { data: { uuid: i } }); FeedItemProps.refresh(); }
 
   return (
     <div className={styles.feedContainer} style={{ width: "-webkit-fill-available" }}>
@@ -27,7 +30,8 @@ export default function FeedItem(FeedItemProps: IFeedItemProps): JSX.Element {
             <button onClick={() => saveLick(i[0])}>save</button>
             <button onClick={() => handleFork(i)}>fork</button>
             <span>transposition<input type='number' min='-24' max='24' placeholder='0' onChange={(e) => handleTranspose(e, i)} /></span>
-            {i[3]} <span id={styles.date}>{i[4]}</span>
+            {i[3]} <span id={styles.date}>{i[4]}</span> 
+            <input type="button" value="delete" id={styles.delete} onClick={() => handleDelete(i[0])}/>
           </span>
         </div>)}
     </div>
